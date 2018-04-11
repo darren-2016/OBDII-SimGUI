920a5c31bbaf4fc62d7e6285c4a913e3c94cc3e4
darren-2016 <darren.j.draper@gmail.com>
2018-04-11 20:59:48 +1000

    Open serial port and issue simple command.

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print 'OBD-II Sim Gui'
+import serial
+
+print ('OBD-II Sim Gui')
+
+ser = serial.Serial()
+ser.baudrate = 38400
+ser.port = '/dev/tty.SLAB_USBtoUART'
+ser.timeout = 4
+ser.open()
+
+print (ser.name)
+ser.write(b'ATZ\r')
+text = ser.readlines()
+print (text)
+
+#text = ser.readline()
+#print (text)
+
+#text = ser.readline()
+#print (text)
+####ser.flush()
+ser.close()
